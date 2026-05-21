9e8b06a1a7f0b93caa197970f87572e110d51e5f
maddie <maddie@madmoss.dev>
2026-05-21 18:53:22 -0500

    rename width and height to window_width and window_height

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use macroquad::{miniquad::conf::Conf, window::next_frame};
 mod grid;
 
-const WIDTH: u16 = 800;
-const HEIGHT: u16 = 600;
+const WINDOW_WIDTH: u16 = 800;
+const WINDOW_HEIGHT: u16 = 600;
 
 fn conf() -> Conf {
     Conf {
         window_title: String::from("Sandbog"),
-        window_width: WIDTH as i32,
-        window_height: HEIGHT as i32,
+        window_width: WINDOW_WIDTH as i32,
+        window_height: WINDOW_HEIGHT as i32,
         window_resizable: false,
         ..Default::default()
     }
