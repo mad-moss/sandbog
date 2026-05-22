a7d9a2b168181d3b7bb4f31d1a519c78fee586d2
maddie <maddie@madmoss.dev>
2026-05-21 20:35:36 -0500

    set up cell base

--- src/grid/cell.rs
+++ src/grid/cell.rs
@@ -7,3 +7,31 @@ pub struct Cell {
     height: u32,
     color: Color,
 }
+
+impl Cell {
+    fn new(x: u32, y: u32, width: u32, height: u32, color: Color) -> Self {
+        Self {
+            x,
+            y,
+            width,
+            height,
+            color,
+        }
+    }
+    pub fn color(&self) -> Color {
+        self.color
+    }
+    pub fn recolor(&mut self, color: Color) {
+        self.color = color;
+    }
+    pub fn split(&mut self, cells_wide: u32, cells_tall: u32) -> Vec<Self> {
+        let width = self.width
+        let children = vec![];
+        for ix in 0..cells_wide {
+            for iy in 0..cells_tall {
+
+            }
+        }
+        children
+    }
+}
