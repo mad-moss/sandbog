89a9dd0c79e3d7eba0af2dfd2db182cbeb4bc03a
maddie <maddie@madmoss.dev>
2026-05-22 18:24:16 -0500

    implement Grid.new() and Grid.init()

--- src/grid.rs
+++ src/grid.rs
@@ -7,6 +7,25 @@ pub struct Grid {
 }
 
 impl Grid {
+    pub fn new(place: Place) -> Self {
+        Self {
+            place,
+            cells: vec![],
+        }
+    }
+    pub fn init(&mut self, cells_wide: u32, cells_tall: u32) {
+        let [x, y, width, height] = [0., 0., self.place.width, self.place.height];
+        let cell_prime = Cell::new(
+            Place {
+                x,
+                y,
+                width,
+                height,
+            },
+            DEFAULT_CELL_COLOR,
+        );
+        self.cells = cell_prime.split(cells_wide, cells_tall);
+    }
     pub fn add_cell(&mut self, cell: Cell) {
         self.cells.push(cell);
     }
