2c274176fca25dc214fd0dac41df2014993e37cf
maddie <maddie@madmoss.dev>
2026-05-22 00:50:49 -0500

    back to using f32, non-guage-type cell data

--- src/grid/cell.rs
+++ src/grid/cell.rs
@@ -1,15 +1,15 @@
 type Color = [u8; 3];
 
 pub struct Cell {
-    x: u32,
-    y: u32,
-    width: u32,
-    height: u32,
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
     color: Color,
 }
 
 impl Cell {
-    fn new(x: u32, y: u32, width: u32, height: u32, color: Color) -> Self {
+    fn new(x: f32, y: f32, width: f32, height: f32, color: Color) -> Self {
         Self {
             x,
             y,
@@ -25,13 +25,13 @@ impl Cell {
         self.color = color;
     }
     pub fn split(self, cells_wide: u32, cells_tall: u32) -> Vec<Self> {
-        let width = self.width * cells_wide;
-        let height = self.height * cells_tall;
+        let width = self.width / cells_wide as f32;
+        let height = self.height / cells_tall as f32;
         let mut cells = vec![];
         for ix in 0..cells_wide {
             for iy in 0..cells_tall {
-                let x = self.x + ix * width;
-                let y = self.y + iy * height;
+                let x = self.x + ix as f32 * width;
+                let y = self.y + iy as f32 * height;
                 let cell = Self::new(x, y, width, height, self.color);
                 cells.push(cell);
             }
