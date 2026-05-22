ad19eaa890371d2a754622139d00afaa76c02df6
maddie <maddie@madmoss.dev>
2026-05-22 14:47:54 -0500

    create and implement load_config function

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,9 @@ struct Config {
 
 #[macroquad::main("Sandbog")]
 async fn main() {
-    let config_string = std::fs::read_to_string(CONFIG_PATH).unwrap();
-    let config: Config = toml::from_str(&config_string).unwrap();
+    let config = load_config(CONFIG_PATH);
+
+    // set window size
     let [window_width, window_height] = config.default_window_size;
     window::request_new_screen_size(window_width as f32, window_height as f32);
 
@@ -23,4 +24,9 @@ async fn main() {
 
         next_frame().await
     }
+}
+
+fn load_config(path: &str) -> Config {
+    let config_string = std::fs::read_to_string(path).unwrap();
+    toml::from_str(&config_string).unwrap()
 }
