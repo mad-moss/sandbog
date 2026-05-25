d1fe0de25e592e0924fdb64baabd51c008f4c6ee
maddie <maddie@madmoss.dev>
2026-05-25 05:07:11 -0500

    clean up sprite constructor

--- src/sprite.rs
+++ src/sprite.rs
@@ -7,13 +7,11 @@ pub struct Sprite {
 
 impl Sprite {
     pub fn blank(x: f32, y: f32, w: u16, h: u16, color: Color) -> Self {
+        let transform = Transform {
+            position: [x, y],
+            absolute_dimensions: [w as f32, h as f32],
+        };
         let texture = Texture::blank(w, h, color);
-        Self {
-            texture,
-            transform: Transform {
-                position: [x, y],
-                absolute_dimensions: [w as f32, h as f32],
-            },
-        }
+        Self { transform, texture }
     }
 }
